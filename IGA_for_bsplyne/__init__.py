aa674df74b5f9ce07044e16c9073131251385568
Dorian BICHET <dbichet@insa-toulouse.fr>
2025-03-21 14:37:22 +0100

    fix pickle issues with init imports

--- IGA_for_bsplyne/__init__.py
+++ IGA_for_bsplyne/__init__.py
@@ -3,6 +3,8 @@
 """
 
 from IGA_for_bsplyne.IGAPatch import IGAPatch
+IGAPatch.__module__ = "IGA_for_bsplyne.IGAPatch"
 from IGA_for_bsplyne.Dirichlet import Dirichlet
 Dirichlet.__module__ = "IGA_for_bsplyne.Dirichlet"
 from IGA_for_bsplyne.ProblemIGA import ProblemIGA
+ProblemIGA.__module__ = "IGA_for_bsplyne.ProblemIGA"
